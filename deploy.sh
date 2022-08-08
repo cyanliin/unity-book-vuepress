e036a42dca75dd9f5c71e36547dca9c9bc06fe23
Vincent Chen <grass527@gmail.com>
2022-08-09 03:52:17 +0800

    feat: 修正 deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:chenyanlintw/unity-book.git master:gh-pages
+git push -f git@github.com:cyanliin/unity-book.git master:gh-pages
 
 cd -
